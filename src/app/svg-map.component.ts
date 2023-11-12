cfa208528b0fa65c8a9b105df1fdbc96cd7eda4f
dariu <dariusstroman@Yahoo.com>
2023-11-11 20:54:46 -0600

    added

--- src/app/svg-map.component.ts
+++ src/app/svg-map.component.ts
@@ -1,16 +1,27 @@
-// svg-map.component.ts
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-svg-map',
   templateUrl: './svg-map.component.html',
   styleUrls: ['./svg-map.component.css']
 })
-export class SvgMapComponent {
-  @Input() countries!: any[]; // Non-null assertion operator
-  @Output() countrySelected = new EventEmitter<string>();
+export class SvgMapComponent implements OnInit {
+  svgContent: SafeHtml | null = null;
+
+  @Output() countrySelected = new EventEmitter<string>(); // Define the EventEmitter
+
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {}
+
+  ngOnInit() {
+    this.http.get('assets/SVG map.svg', { responseType: 'text' })
+      .subscribe(svg => {
+        this.svgContent = this.sanitizer.bypassSecurityTrustHtml(svg);
+      });
+  }
 
   selectCountry(countryCode: string) {
-    this.countrySelected.emit(countryCode);
+    this.countrySelected.emit(countryCode); // Emit the selected country code
   }
 }
